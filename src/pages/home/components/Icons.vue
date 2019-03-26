4aa953ca3ec91cc0ff472d693c45436fe6588a0c
Yuerbaibai <907998075@qq.com>
2019-03-26 18:22:39 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+         <div class="icon" v-for="item of page" :key="item.id">
+           <div class="icon-img">
+             <img class="icon-img-content" :src="item.imgUrl"/>
+           </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+          </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {id: '0001', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0002', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0003', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0004', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0005', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0006', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0007', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0008', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'},
+        {id: '0009', imgUrl: '//s.qunarzz.com/vacation_react/outbround/group.png', desc: '景点门票'}
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+.icons >>> .swiper-container
+  height:0
+  padding-bottom: 50%
+  .icon
+    height: 0
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        height: 100%
+        display: block
+        margin:0 auto
+  .icon-desc
+    position: absolute
+    bottom: 0
+    left: 0
+    right: 0
+    height: .44rem
+    line-height: .44rem
+    text-align: center
+    color: $darkTextColor
+    ellipsis()
+</style>
